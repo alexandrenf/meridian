cb9bf84af4f9043138727ddf23bcda735d39d372
Alex <5628895+alexandrenf@users.noreply.github.com>
2025-04-03 22:34:25 -0300

    chore: update pnpm-lock.yaml and package.json to reflect new versions of @tailwindcss/typography and postcss, and refactor CSS imports in main.css

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/components/**/*.{js,vue,ts}',
+    './src/layouts/**/*.vue',
+    './src/pages/**/*.vue',
+    './src/plugins/**/*.{js,ts}',
+    './src/app.vue',
+    './src/error.vue',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+} 
